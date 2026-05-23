168f25f6824a7999335e8975ab0f38a2b2b01aca
Baraq Lipshitz <baraqlip@gmail.com>
2026-05-23 12:11:07 +0200

    add mac uv environment, finish download script

--- commands/download_hf_data.sh
+++ commands/download_hf_data.sh
@@ -1,7 +1,15 @@
-# User variables
-
 # Script variables
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+# User variables
+# ----------
+HF_ORG="ETHRC"
+HF_REPO="robot-learning-fs26"
+DOWNLOAD_ROOT="$REPO_ROOT/data/hf_datasets/$HF_REPO"
+# ----------
+
+# Script variables
+HF_PATH="$HF_ORG/$HF_REPO"
 
 # Setup
 cd "$REPO_ROOT"
@@ -10,3 +18,8 @@ cd "$REPO_ROOT/model/"
 uv sync
 source .venv/bin/activate
 cd "$REPO_ROOT"
+
+echo "You are signed in on Hugging Face as:"
+hf auth whoami
+
+hf download "$HF_PATH" --repo-type dataset --local-dir "$DOWNLOAD_ROOT"
